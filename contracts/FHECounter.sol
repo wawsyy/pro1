c0aca6a3762cf4993155187f953ad2fc98dc0236
wawsyy <shiyu689@qq.com>
2025-11-01 16:40:00 -0800

    feat: add ownership and access control to counter contract

--- contracts/FHECounter.sol
+++ contracts/FHECounter.sol
@@ -7,10 +7,20 @@ import {SepoliaConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 /// @title A simple FHE counter contract
 contract FHECounter is SepoliaConfig {
     euint32 private _count;
+    address private _owner;
 
     event CounterIncremented(address indexed user);
     event CounterDecremented(address indexed user);
     event CounterReset(address indexed user);
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "FHECounter: caller is not the owner");
+        _;
+    }
+
+    constructor() {
+        _owner = msg.sender;
+    }
 
     /// @notice Returns the current count
     function getCount() external view returns (euint32) {
@@ -46,12 +56,17 @@ contract FHECounter is SepoliaConfig {
     }
 
     /// @notice Resets the counter to zero
-    function reset() external {
+    function reset() external onlyOwner {
         _count = FHE.asEuint32(0);
         
         FHE.allowThis(_count);
         FHE.allow(_count, msg.sender);
         
         emit CounterReset(msg.sender);
+    }
+
+    /// @notice Returns the owner address
+    function owner() external view returns (address) {
+        return _owner;
     }
 }
